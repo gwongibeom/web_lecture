b09203e9686fbe79082af0a95bfe656b05e0f5f2
gwongibeom <k201gun@kakao.com>
2022-12-28 14:24:00 +0900

    add learn javjascript folder

---
+++ day3/htmlcssjs/app.js
@@ -0,0 +1,10 @@
+var global = "전역";
+if (global === "전역") {
+    var global = "지역";
+    console.log(global);
+}
+console.log(global);
+
+const nigga = "have2die";
+
+console.log("nigga = " + nigga);
