286cc04cc9fa2e6f9964151663c8658521111baf
gwongibeom <k201gun@kakao.com>
2022-12-30 13:31:56 +0900

    learn express

--- day5/5.2/app.js
+++ day5/5.2/app.js
@@ -2,10 +2,15 @@ const express = require("express")
 
 const app = express()
 
+app.get("/", (req, res) => {
+    res.send("응애")
+    console.log("You`re in home!")
+})
+
 app.get("/home", () => {
     console.log("You`re in home!")
 })
 
-app.listen(3000, () => {
+app.listen(3000, (res, req) => {
     console.log("Hello world!")
 })
